d3ae85b266711aa02b8e7bf5a1b74ee061849299
Roy <Roy@Roy-VAIO>
2015-01-25 16:31:49 -0800

    Added code to enable quote to appear Signed-off-by:Roy <Roy@Roy-VAIO>

--- application/views/justone.php
+++ application/views/justone.php
@@ -1,7 +1,9 @@
 <div class="row">
+    {authors}
     <div class="span3"><img src="/data/{mug}" title="{who}"/></div>
     <div class="span8 offset1">
         <p class="lead">{what}</p><br/>
         <p class="text-right">{who}</p>
     </div>
+    {/authors}
 </div>
