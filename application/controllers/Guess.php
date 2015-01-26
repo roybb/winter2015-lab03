559d6d1c4a695c9fb51cafd9aa3f569b9ca6b448
Roy <Roy@Roy-VAIO>
2015-01-25 16:22:53 -0800

    Added Controller Guess.php Signed-off-by:Roy <Roy@Roy-VAIO>

---
+++ application/controllers/Guess.php
@@ -0,0 +1,31 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Guess extends Application {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    //-------------------------------------------------------------
+    //  The normal pages
+    //-------------------------------------------------------------
+
+    function index() {
+        $this->data['pagebody'] = 'justone';    // this is the view we want shown
+        // build the list of authors, to pass on to our view
+        $source = $this->quotes->get(4);
+        //foreach ($source as $record) {
+        $authors = array (array('who' => $source['who'], 'mug' => $source['mug'], 'href' => $source['where'], 'what' => $source['what']));
+        //}
+        $this->data['authors'] = $authors;
+
+        $this->render();
+    }
+
+}
